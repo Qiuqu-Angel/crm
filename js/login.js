2df8afbfe3fff6e9c6553a517c2d6a8c7eb284e5
QiuQu-Angel <865231701@qq.com>
2020-08-31 14:34:09 +0800

    登录完成

---
+++ js/login.js
@@ -0,0 +1,47 @@
+$(function () {
+    //登录功能
+    // $(".submit").click(function (e) {
+    //     let account = $(".userName").val().trim()
+    //     let password = $(".userPass").val().trim()
+
+    //     if (account === "" || password === "") {
+    //         alert("账号或密码不能为空")
+    //         return
+    //     }
+    //     password = md5(password);
+    //     // console.log(account, password);
+
+    //     //发出ajax请求
+    //     axios.post("/user/login", {
+    //         account,
+    //         password
+    //     }).then(res=>{
+    //         console.log(res);   
+    //     }
+    //     ).catch(err=> {
+    //         console.log(err);
+    //     })
+    // })
+
+    //登录功能
+    $(".submit").click(async function (e) {
+        let account = $(".userName").val().trim();
+        let password = $(".userPass").val().trim();
+
+        if(account === "" || password === ""){
+            alert("账号或密码不能为空")
+            return
+        }
+        password = md5(password);
+
+        //发出ajax请求
+        let res = await axios.post("/user/login",{account,password})
+       
+       if(parseInt(res.code)===0){
+           alert("登录成功")
+           window.location.href="index.html";
+           return
+       }
+        alert("用户名或密码出错")
+    })
+})
